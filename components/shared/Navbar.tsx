48a35a6dfd56f9d7d904972e2a03b4a7beceac7a
Casprine Assempah <casprine.001@gmail.com>
2022-01-01 23:22:58 +0000

    add back button

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,14 +1,23 @@
 import styled from 'styled-components';
 import Router, { useRouter } from 'next/router';
 import Link from 'next/link';
+import { sand } from '@radix-ui/colors';
+import { route } from 'next/dist/server/router';
 
 export const Navbar = () => {
   const router = useRouter();
 
   const isActiveLink = (link: string) => (router.pathname.includes(link) ? 'active-nav-link' : '');
 
+  const isHomePage = router.pathname === '/';
+
   return (
     <StyledHeader>
+      {!isHomePage && (
+        <div onClick={() => router.back()}>
+          <p className="home-link">Back</p>
+        </div>
+      )}
       <div className="nav-links flex">
         <Link href="/writing" passHref>
           <a className={`nav-link ${isActiveLink('writing')}`}>Writing</a>
@@ -27,9 +36,10 @@ export const Navbar = () => {
 const StyledHeader = styled.header`
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: flex-start;
 
   .nav-links {
+    margin-left: auto;
     a {
       font-size: 16px;
       padding: 0 10px;
@@ -41,6 +51,18 @@ const StyledHeader = styled.header`
 
     .active-nav-link {
       text-decoration: line-through;
+    }
+  }
+
+  .home-link {
+    font-family: 'PTSerif';
+    font-size: 17px;
+    cursor: pointer;
+
+    :hover {
+      background: linear-gradient(90deg, rgb(255, 154, 158) 0%, rgb(254, 207, 239) 99%, rgb(254, 207, 239) 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
     }
   }
 `;
